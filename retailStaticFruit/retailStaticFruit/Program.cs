4f2ce8b9ab1f4f8c5ea6aea41a396797b3de7858
RobvanEmbden <61789232+RobvanEmbden@users.noreply.github.com>
2021-03-07 17:38:35 +0100

    Add files via upload

---
+++ retailStaticFruit/retailStaticFruit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace retailStaticFruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit fruit = new Fruit { Kind = "Macintosh", Cost = 0.5, Amount = 12 };
+            Console.WriteLine("Fruit: {0} {1}", Fruit.FruitType, fruit.GetFruit());
+        }
+        class Fruit
+        {
+            public static string FruitType
+            {
+                get { return "Apple"; }
+            }
+            public string Kind { get; set; }
+            public double Cost { get; set; }
+            public double Amount { get; set; }
+
+            public string GetFruit()
+            {
+                return this.Kind + " $" + this.Cost + " each, $" + this.Amount + " per dozen";
+            }
+        }
+    }
+}
